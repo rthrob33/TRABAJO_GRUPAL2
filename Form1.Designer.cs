9d13d4b5c0b53017b6fa949f4ec9fe9880bbd657
Roberth Melendez <alexanderrms33@gmail.com>
2025-08-16 16:40:54 -0600

    PRUEBA FINAL

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -20,117 +20,128 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			txtBoxTarea = new TextBox();
-			cbPrioridad = new ComboBox();
-			btnAgregar = new Button();
-			txtBoxBuscar = new TextBox();
-			btnBuscar = new Button();
-			lblTarea = new Label();
-			lblPrioridad = new Label();
-			flowPanelTodoList = new FlowLayoutPanel();
-			SuspendLayout();
-			// 
-			// txtBoxTarea
-			// 
-			txtBoxTarea.Location = new Point(42, 51);
-			txtBoxTarea.Name = "txtBoxTarea";
-			txtBoxTarea.PlaceholderText = "Indique el nombre de su tarea";
-			txtBoxTarea.Size = new Size(550, 27);
-			txtBoxTarea.TabIndex = 0;
-			// 
-			// cbPrioridad
-			// 
-			cbPrioridad.FormattingEnabled = true;
-			cbPrioridad.Items.AddRange(new object[] { "Alta", "Media", "Baja" });
-			cbPrioridad.Location = new Point(616, 52);
-			cbPrioridad.Name = "cbPrioridad";
-			cbPrioridad.Size = new Size(187, 28);
-			cbPrioridad.TabIndex = 1;
-			// 
-			// btnAgregar
-			// 
-			btnAgregar.Location = new Point(823, 51);
-			btnAgregar.Name = "btnAgregar";
-			btnAgregar.Size = new Size(94, 29);
-			btnAgregar.TabIndex = 2;
-			btnAgregar.Text = "Agregar";
-			btnAgregar.UseVisualStyleBackColor = true;
-			btnAgregar.Click += onBtnAgregarClick;
-			// 
-			// txtBoxBuscar
-			// 
-			txtBoxBuscar.Location = new Point(617, 117);
-			txtBoxBuscar.Name = "txtBoxBuscar";
-			txtBoxBuscar.PlaceholderText = "Nombre de la tarea ...";
-			txtBoxBuscar.Size = new Size(187, 27);
-			txtBoxBuscar.TabIndex = 3;
-			// 
-			// btnBuscar
-			// 
-			btnBuscar.Location = new Point(824, 117);
-			btnBuscar.Name = "btnBuscar";
-			btnBuscar.Size = new Size(94, 29);
-			btnBuscar.TabIndex = 4;
-			btnBuscar.Text = "Buscar";
-			btnBuscar.UseVisualStyleBackColor = true;
-			// 
-			// lblTarea
-			// 
-			lblTarea.AutoSize = true;
-			lblTarea.Location = new Point(48, 34);
-			lblTarea.Name = "lblTarea";
-			lblTarea.Size = new Size(44, 20);
-			lblTarea.TabIndex = 5;
-			lblTarea.Text = "Tarea";
-			// 
-			// lblPrioridad
-			// 
-			lblPrioridad.AutoSize = true;
-			lblPrioridad.Location = new Point(623, 34);
-			lblPrioridad.Name = "lblPrioridad";
-			lblPrioridad.Size = new Size(70, 20);
-			lblPrioridad.TabIndex = 6;
-			lblPrioridad.Text = "Prioridad";
-			// 
-			// flowPanelTodoList
-			// 
-			flowPanelTodoList.Location = new Point(28, 156);
-			flowPanelTodoList.Name = "flowPanelTodoList";
-			flowPanelTodoList.Size = new Size(878, 386);
-			flowPanelTodoList.TabIndex = 7;
-			// 
-			// Form1
-			// 
-			AutoScaleDimensions = new SizeF(8F, 20F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(930, 563);
-			Controls.Add(flowPanelTodoList);
-			Controls.Add(lblPrioridad);
-			Controls.Add(lblTarea);
-			Controls.Add(btnBuscar);
-			Controls.Add(txtBoxBuscar);
-			Controls.Add(btnAgregar);
-			Controls.Add(cbPrioridad);
-			Controls.Add(txtBoxTarea);
-			Name = "Form1";
-			Padding = new Padding(6);
-			Text = "Control de Tareas";
-			Click += onBtnAgregarClick;
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtBoxTarea = new TextBox();
+            cbPrioridad = new ComboBox();
+            btnAgregar = new Button();
+            txtBoxBuscar = new TextBox();
+            btnBuscar = new Button();
+            lblTarea = new Label();
+            lblPrioridad = new Label();
+            flowPanelTodoList = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // txtBoxTarea
+            // 
+            txtBoxTarea.Location = new Point(63, 76);
+            txtBoxTarea.Margin = new Padding(4);
+            txtBoxTarea.Name = "txtBoxTarea";
+            txtBoxTarea.PlaceholderText = "Indique el nombre de su tarea";
+            txtBoxTarea.Size = new Size(823, 35);
+            txtBoxTarea.TabIndex = 0;
+            // 
+            // cbPrioridad
+            // 
+            cbPrioridad.FormattingEnabled = true;
+            cbPrioridad.Items.AddRange(new object[] { "Alta", "Media", "Baja" });
+            cbPrioridad.Location = new Point(924, 78);
+            cbPrioridad.Margin = new Padding(4);
+            cbPrioridad.Name = "cbPrioridad";
+            cbPrioridad.Size = new Size(278, 38);
+            cbPrioridad.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(1234, 76);
+            btnAgregar.Margin = new Padding(4);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(141, 44);
+            btnAgregar.TabIndex = 2;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += onBtnAgregarClick;
+            // 
+            // txtBoxBuscar
+            // 
+            txtBoxBuscar.Location = new Point(926, 176);
+            txtBoxBuscar.Margin = new Padding(4);
+            txtBoxBuscar.Name = "txtBoxBuscar";
+            txtBoxBuscar.PlaceholderText = "Nombre de la tarea ...";
+            txtBoxBuscar.Size = new Size(278, 35);
+            txtBoxBuscar.TabIndex = 3;
+            txtBoxBuscar.TextChanged += txtBoxBuscar_TextChanged;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(1236, 176);
+            btnBuscar.Margin = new Padding(4);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(141, 44);
+            btnBuscar.TabIndex = 4;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            btnBuscar.Click += txtBoxBuscar_TextChanged;
+            // 
+            // lblTarea
+            // 
+            lblTarea.AutoSize = true;
+            lblTarea.Location = new Point(72, 51);
+            lblTarea.Margin = new Padding(4, 0, 4, 0);
+            lblTarea.Name = "lblTarea";
+            lblTarea.Size = new Size(62, 30);
+            lblTarea.TabIndex = 5;
+            lblTarea.Text = "Tarea";
+            // 
+            // lblPrioridad
+            // 
+            lblPrioridad.AutoSize = true;
+            lblPrioridad.Location = new Point(934, 51);
+            lblPrioridad.Margin = new Padding(4, 0, 4, 0);
+            lblPrioridad.Name = "lblPrioridad";
+            lblPrioridad.Size = new Size(96, 30);
+            lblPrioridad.TabIndex = 6;
+            lblPrioridad.Text = "Prioridad";
+            // 
+            // flowPanelTodoList
+            // 
+            flowPanelTodoList.Location = new Point(42, 234);
+            flowPanelTodoList.Margin = new Padding(4);
+            flowPanelTodoList.Name = "flowPanelTodoList";
+            flowPanelTodoList.Size = new Size(1317, 579);
+            flowPanelTodoList.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1395, 844);
+            Controls.Add(flowPanelTodoList);
+            Controls.Add(lblPrioridad);
+            Controls.Add(lblTarea);
+            Controls.Add(btnBuscar);
+            Controls.Add(txtBoxBuscar);
+            Controls.Add(btnAgregar);
+            Controls.Add(cbPrioridad);
+            Controls.Add(txtBoxTarea);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Padding = new Padding(9);
+            Text = "Control de Tareas";
+            Click += onBtnAgregarClick;
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private TextBox txtBoxTarea;
+        private TextBox txtBoxTarea;
 		private ComboBox cbPrioridad;
 		private Button btnAgregar;
 		private TextBox txtBoxBuscar;
--- Form1.cs
+++ Form1.cs
@@ -2,98 +2,121 @@ using TodoList.Clases;
 
 namespace TodoList
 {
-	public partial class Form1 : Form
-	{
-		List<Tarea> tareas = new List<Tarea>();
-		public Form1()
-		{
-			InitializeComponent();
-		}
+    public partial class Form1 : Form
+    {
+        List<Tarea> tareas = new List<Tarea>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-		public void onBtnAgregarClick(object sender, EventArgs e)
-		{
-			string nombreTarea = txtBoxTarea.Text;
-			string prioridadSeleccionada = cbPrioridad.SelectedItem.ToString();
-			Prioridad prioridadTarea;
-			switch (prioridadSeleccionada)
-			{
-				case "Alta":
-					prioridadTarea = Prioridad.Alta;
-					break;
-				case "Media":
-					prioridadTarea = Prioridad.Media;
-					break;
-				default:
-					prioridadTarea = Prioridad.Baja;
-					break;
+        public void onBtnAgregarClick(object sender, EventArgs e)
+        {
+            string nombreTarea = txtBoxTarea.Text;
+            string prioridadSeleccionada = cbPrioridad.SelectedItem.ToString();
+            Prioridad prioridadTarea;
+            switch (prioridadSeleccionada)
+            {
+                case "Alta":
+                    prioridadTarea = Prioridad.Alta;
+                    break;
+                case "Media":
+                    prioridadTarea = Prioridad.Media;
+                    break;
+                default:
+                    prioridadTarea = Prioridad.Baja;
+                    break;
 
-			}
+            }
 
-			Tarea nuevaTarea = new Tarea { Nombre = nombreTarea, Prioridad = prioridadTarea };
+            Tarea nuevaTarea = new Tarea { Nombre = nombreTarea, Prioridad = prioridadTarea };
 
-			tareas.Add(nuevaTarea);
+            tareas.Add(nuevaTarea);
 
-			txtBoxTarea.Text = "";
-			cbPrioridad.ResetText();
+            txtBoxTarea.Text = "";
+            cbPrioridad.ResetText();
 
-			renderizarTareas();
-		}
+            renderizarTareas();
+        }
 
-		public void renderizarTareas()
-		{
-			flowPanelTodoList.Controls.Clear();
-			foreach (Tarea tarea in tareas)
-			{
-				Panel tarjeta = crearTarjeta(tarea);
-				flowPanelTodoList.Controls.Add(tarjeta);
-			}
-		}
+        public void renderizarTareas()
+        {
+            flowPanelTodoList.Controls.Clear();
+            foreach (Tarea tarea in tareas)
+            {
+                Panel tarjeta = crearTarjeta(tarea);
+                flowPanelTodoList.Controls.Add(tarjeta);
+            }
+        }
 
-		public Panel crearTarjeta(Tarea tarea) {
-			Panel tarjeta = new Panel();
-			tarjeta.Size = new Size(200, 100);
-			tarjeta.Padding = new Padding(5);
-			tarjeta.Margin = new Padding(5);
-			tarjeta.BackColor = Color.White;
-			tarjeta.Tag = tarea;
+        public Panel crearTarjeta(Tarea tarea)
+        {
+            Panel tarjeta = new Panel();
+            tarjeta.Size = new Size(200, 100);
+            tarjeta.Padding = new Padding(5);
+            tarjeta.Margin = new Padding(5);
+            tarjeta.BackColor = Color.White;
+            tarjeta.Tag = tarea;
 
-			Label nombreTarea = new Label();
-			nombreTarea.AutoSize = true;
-			nombreTarea.Text = $"{tarea.Nombre}";
-			nombreTarea.Font = new Font("Segoe UI", 9, FontStyle.Bold);
-			nombreTarea.Dock = DockStyle.Top;
-			nombreTarea.Margin = new Padding(3, 3, 3, 3);
+            Label nombreTarea = new Label();
+            nombreTarea.AutoSize = true;
+            nombreTarea.Text = $"{tarea.Nombre}";
+            nombreTarea.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+            nombreTarea.Dock = DockStyle.Top;
+            nombreTarea.Margin = new Padding(3, 3, 3, 3);
 
-			Label prioridad = new Label();
-			prioridad.AutoSize = true;
-			prioridad.Text = tarea.Prioridad.ToString();
-			prioridad.Font = new Font("Segoe UI", 9);
-			prioridad.Dock = DockStyle.Top;
-			prioridad.Margin = new Padding(3,50,3, 3);
+            Label prioridad = new Label();
+            prioridad.AutoSize = true;
+            prioridad.Text = tarea.Prioridad.ToString();
+            prioridad.Font = new Font("Segoe UI", 9);
+            prioridad.Dock = DockStyle.Top;
+            prioridad.Margin = new Padding(3, 50, 3, 3);
 
 
-			switch (tarea.Prioridad)
-			{
-				case Prioridad.Alta:
-					prioridad.ForeColor = Color.Red;
-					break;
-				case Prioridad.Media:
-					prioridad.ForeColor = Color.Orange;
-					break;
-				default: // Baja
-					prioridad.ForeColor = Color.Green;
-					break;
-			}
+            switch (tarea.Prioridad)
+            {
+                case Prioridad.Alta:
+                    prioridad.ForeColor = Color.Red;
+                    break;
+                case Prioridad.Media:
+                    prioridad.ForeColor = Color.Orange;
+                    break;
+                default: // Baja
+                    prioridad.ForeColor = Color.Green;
+                    break;
+            }
 
-			tarjeta.Controls.Add(prioridad);
-			tarjeta.Controls.Add(nombreTarea);
+            tarjeta.Controls.Add(prioridad);
+            tarjeta.Controls.Add(nombreTarea);
 
-			return tarjeta;
-		}
+            return tarjeta;
+        }
 
-		private void panel1_Paint(object sender, PaintEventArgs e)
-		{
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
 
-		}
-	}
+
+        }
+
+        private void txtBoxBuscar_TextChanged(object sender, EventArgs e)
+        {
+            string textoBusqueda = txtBoxBuscar.Text.ToLower();
+            flowPanelTodoList.Controls.Clear();
+            foreach (Tarea tarea in tareas)
+            {
+                if (tarea.Nombre.ToLower().Contains(textoBusqueda))
+                {
+                    Panel tarjeta = crearTarjeta(tarea);
+                    flowPanelTodoList.Controls.Add(tarjeta);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbPrioridad.Items.Add("Alta");
+            cbPrioridad.Items.Add("Media");
+            cbPrioridad.Items.Add("Baja");
+            cbPrioridad.SelectedIndex = 0; // Selecciona "Baja" por defecto
+        }
+    }
 }
